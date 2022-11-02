2e568dabdbc885e568d5c52e628fc9c57360f446
Tomas-Leiva-Duran <101905549+Tomas-Leiva-Duran@users.noreply.github.com>
2022-11-02 17:51:20 -0300

    oki

--- src/app/inicio/inicio-routing.module.ts
+++ src/app/inicio/inicio-routing.module.ts
@@ -1,32 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//import { UsuarioPage } from '../usuario/usuario.page';
+import { UsuarioPage } from '../usuario/usuario.page';
 
 import { InicioPage } from './inicio.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: InicioPage,
-    children:[
-      {
-        path: 'usuario',
-        loadChildren: () => import('../usuario/usuario.module').then( m => m.UsuarioPageModule)
-      },
-      {
-        path: '',
-        redirectTo:'/inicio/inicio',
-        pathMatch: 'full'
-      }
-
-    ]
+    component: InicioPage
   },
   {
-    path: '',
-    redirectTo:'/inicio/inicio',
-    pathMatch: 'full'
-  }
-
+     path: 'usuario',
+    loadChildren: () => import('../usuario/usuario.module').then( m => m.UsuarioPageModule)
+  },
 ];
 
 @NgModule({
